71144733661fb5e4d7b4e36f0fb646ae78614a7a
u03013112@hotmail.com <u03013112@hotmail.com>
2021-01-20 16:02:07 +0800

    代码摘抄，还未整理

---
+++ cam.py
@@ -0,0 +1,34 @@
+from cv2 import cv2
+import sys
+import time
+
+#变成一个纯粹的监视器
+if __name__ =='__main__':
+    if len(sys.argv) <2:
+            print('input ip of netcam!')
+            exit()
+
+    while True:
+        try:
+            print('try 2 connect rtsp')
+            camera=cv2.VideoCapture(sys.argv[1])
+        except Exception as e:
+            print('Error:',e)
+            continue
+        print('connected '+sys.argv[1]+' successed!')
+
+        while True:
+            if camera.isOpened():
+                success,frame = camera.read()
+                if success==False:
+                    print('camera read err!')
+                    break
+
+                cv2.imshow("cam", frame)
+                key = cv2.waitKey(1)
+            else:
+                print('camera not open!')
+                break
+
+        camera.release()
+        cv2.destroyAllWindows()
