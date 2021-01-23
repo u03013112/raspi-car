22ee288d84142d9d0db27a29e564d6580d65ce0e
u03013112@hotmail.com <u03013112@hotmail.com>
2021-01-24 01:49:39 +0800

    fuxk

--- raspi/exec.py
+++ raspi/exec.py
@@ -1,14 +1,11 @@
-
-from subprocess import call
+import os
 
 class EXEC:
     def __init__(self):
         pass
     def exec(self,cmd):
         prefix = "nsenter --mount=/host/proc/1/ns/mnt "
-        call(prefix + cmd)
-        # shell=True
-        print()
+        os.system(prefix + cmd)
     def openCamera(self):
         cmd = "raspivid -l -o tcp://0.0.0.0:8888 -hf -vf -t 0 -w 640 -h 480 -fps 15 &"
         self.exec(cmd)
@@ -17,4 +14,6 @@ class EXEC:
 
 if __name__ == '__main__':
     exec = EXEC()
+    #exec.openCamera()
+    exec.closeCamera()
     
