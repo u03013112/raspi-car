07b766c7ae16af2a8f66c76cc356b228b8adc470
u03013112@hotmail.com <u03013112@hotmail.com>
2021-01-24 00:35:35 +0800

    add exec

---
+++ raspi/exec.py
@@ -0,0 +1,13 @@
+
+import os
+
+class EXEC:
+    def __init__(self):
+        pass
+    def exec(self,cmd):
+        val = os.system('ls -al')
+        print(val)
+
+if __name__ == '__main__':
+    exec = EXEC()
+    exec.exec("nsenter --mount=/host/proc/1/ns/mnt raspivid -l -o tcp://0.0.0.0:8888 -hf -vf -t 0 -w 640 -h 480 -fps 20")        
