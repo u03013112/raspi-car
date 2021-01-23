7d1d82944d0b9efca73e20d3a86a89702f375f81
u03013112@hotmail.com <u03013112@hotmail.com>
2021-01-24 01:12:01 +0800

    add exec

--- raspi/exec.py
+++ raspi/exec.py
@@ -1,13 +1,22 @@
 
-import os
+from subprocess import call
 
 class EXEC:
-    def __init__(self):
-        pass
+    def __init__(self,w=640,h=480,fps=15):
+        self.w = w
+        self.h = h
+        self.fps = fps
+
     def exec(self,cmd):
-        val = os.system(cmd)
-        print(val)
+        prefix = "nsenter --mount=/host/proc/1/ns/mnt "
+        call(prefix + cmd) 
+        # shell=True
+    def openCamera(self):
+        cmd = "raspivid -l -o tcp://0.0.0.0:8888 -hf -vf -t 0 -w "+self.w+" -h "+self.h+" -fps "+self.fps +" &"
+        self.exec(cmd)
+    def closeCamera(self):
+        self.exec("pkill raspivid")
 
 if __name__ == '__main__':
     exec = EXEC()
-    exec.exec("nsenter --mount=/host/proc/1/ns/mnt raspivid -l -o tcp://0.0.0.0:8888 -hf -vf -t 0 -w 640 -h 480 -fps 20 &")
+    
--- raspi/main.py
+++ raspi/main.py
@@ -1,10 +1,12 @@
 import eventlet
 import socketio
 from gpio import GPIO
+from exec import EXEC
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 gpio = GPIO()
+exec = EXEC()
 
 @sio.event
 def connect(sid, environ):
@@ -14,6 +16,11 @@ def connect(sid, environ):
 def ctrl(sid, data):
     print('ctrl ', data)
     gpio.set(data)
+
+@sio.event
+def exec(sid, data):
+    print('exec ', data)
+    exec.exec(data.cmd)
 
 @sio.event
 def disconnect(sid):
