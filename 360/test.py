6d0dfad1115e0895af2d29b0a362c00f9d5c778d
u03013112 <u03013112@hotmail.com>
2022-10-24 18:28:10 +0800

    1

---
+++ 360/test.py
@@ -0,0 +1,24 @@
+import os
+import cv2 
+import Equirec2Perspec as E2P 
+
+if __name__ == '__main__':
+    equ = E2P.Equirectangular('image34.jpg')    # Load equirectangular image
+    
+    #
+    # FOV unit is degree 
+    # theta is z-axis angle(right direction is positive, left direction is negative)
+    # phi is y-axis angle(up direction positive, down direction negative)
+    # height and width is output image dimension 
+    #
+    img = equ.GetPerspective(90, 0, -30, 720, 1080) # Specify parameters(FOV, theta, phi, height, width)
+    cv2.imwrite('1.jpg',img)
+
+    img = equ.GetPerspective(90, 90, -30, 720, 1080) # Specify parameters(FOV, theta, phi, height, width)
+    cv2.imwrite('2.jpg',img)
+
+    img = equ.GetPerspective(90, 180, -30, 720, 1080) # Specify parameters(FOV, theta, phi, height, width)
+    cv2.imwrite('3.jpg',img)
+
+    img = equ.GetPerspective(90, -90, -30, 720, 1080) # Specify parameters(FOV, theta, phi, height, width)
+    cv2.imwrite('4.jpg',img)
